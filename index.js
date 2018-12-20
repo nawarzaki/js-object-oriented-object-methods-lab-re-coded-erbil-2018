e64b280fd15c6636258f5bc012a42e17d4dff930
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-12-20 07:37:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function BoardMembe(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+
+let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
