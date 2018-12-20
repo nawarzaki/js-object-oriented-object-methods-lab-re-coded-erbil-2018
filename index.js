ac4dbf3607842f06a42cbbeaa3529336024a9969
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-12-20 08:25:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+
+let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+veto = function(){
+  console.log('No, I must disagree');
+}
